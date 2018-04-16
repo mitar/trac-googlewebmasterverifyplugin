e781fbd5c83c77a59c8d4768b09663c1e4eb8ad7
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2018-04-16 18:32:17 +0000

    TracGoogleWebmasterVerifyPlugin 0.3: Conform to PEP8
    git-svn-id: https://trac-hacks.org/svn/googlewebmasterverifyplugin@17131 7322e99d-02ea-0310-aa39-e9a107903beb

--- 0.11/setup.py
+++ 0.11/setup.py
@@ -3,25 +3,19 @@
 
 from setuptools import setup
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
-
-
-
 setup(
-    name = 'TracGoogleWebmasterVerifyPlugin',
-    version = '0.2',
-    packages = ['tracgooglewebmasterverify'],
-    author = 'Martin Scharrer',
-    author_email = 'martin@scharrer-online.de',
-    description = "GoogleWebmasterVerify Plugin for Trac",
-    url = 'http://www.trac-hacks.org/wiki/GoogleWebmasterVerifyPlugin',
-    license      = 'GPLv3',
-    zip_safe     = False,
-    keywords = 'trac google webmaster verify wiki plugin',
-    classifiers = ['Framework :: Trac'],
-    entry_points = {'trac.plugins': ['tracgooglewebmasterverify.plugin = tracgooglewebmasterverify.plugin']}
+    name='TracGoogleWebmasterVerifyPlugin',
+    version='0.3',
+    packages=['tracgooglewebmasterverify'],
+    author='Martin Scharrer',
+    author_email='martin@scharrer-online.de',
+    description="GoogleWebmasterVerify Plugin for Trac",
+    url='https://www.trac-hacks.org/wiki/GoogleWebmasterVerifyPlugin',
+    license='GPLv3',
+    zip_safe=False,
+    install_requires=['Trac'],
+    keywords='trac google webmaster verify wiki plugin',
+    classifiers=['Framework :: Trac'],
+    entry_points={'trac.plugins': [
+        'tracgooglewebmasterverify.plugin = tracgooglewebmasterverify.plugin']}
 )
-
--- 0.11/tracgooglewebmasterverify/plugin.py
+++ 0.11/tracgooglewebmasterverify/plugin.py
@@ -5,41 +5,39 @@
  This is Free Software under the BSD license.
 """
 
-__url__      = ur"$URL$"[6:-2]
-__author__   = ur"$Author$"[9:-2]
-__revision__ = int("0" + ur"$Rev$"[6:-2])
-__date__     = ur"$Date$"[7:-2]
-
-from trac.core import *
-
-from  trac.web.api     import  IRequestFilter, IRequestHandler, RequestDone
-from  trac.web.chrome  import  add_link,add_stylesheet,add_script #,add_meta
-from  trac.config      import  ListOption
+from trac.config import ListOption
+from trac.core import Component, implements
+from trac.web.api import IRequestHandler, RequestDone
+from trac.web.chrome import add_meta
 
 
 class GoogleWebmasterVerifyPlugin(Component):
-    """Simply plugin to return verify webpages for Google Webmaster Service."""
-    implements( IRequestHandler ) #, IRequestFilter )
-    vlist = ListOption('googlewebmasterverify', 'verify', doc="Verification code(s)")
-    #mvlist = ListOption('googlewebmasterverify', 'meta_verify', doc="Verification code(s) for meta tag verifcation.")
+    """Simply plugin to return verify webpages for Google Webmaster Service.
+    """
 
-    ### methods for IRequestHandler
+    implements(IRequestHandler)
+
+    vlist = ListOption('googlewebmasterverify', 'verify',
+                       doc="Verification code(s)")
+
+    # IRequestHandler methods
+
     def match_request(self, req):
         """Check if requested URL is '/googleXX..XX.html' where XX..XX is in
         config list."""
         try:
-          path  = req.path_info
-          if path.startswith("/google") and path.endswith(".html"):
-            path = path[7:-5]
-          else:
-            return False
+            path = req.path_info
+            if path.startswith("/google") and path.endswith(".html"):
+                path = path[7:-5]
+            else:
+                return False
 
-          if path in self.vlist:
-            return True
-          else:
-            return False
+            if path in self.vlist:
+                return True
+            else:
+                return False
         except:
-          return False
+            return False
 
     def process_request(self, req):
         """Send an empty HTTP 200 OK response."""
@@ -53,11 +51,8 @@ class GoogleWebmasterVerifyPlugin(Component):
         return handler
 
     def post_process_request(self, req, template, data, content_type):
-        self.env.log.debug("add_meta")
         if self.mvlist:
-          for code in self.mvlist:
-            self.env.log.debug("add_meta: " + code)
-            add_meta(req,code,name="google-site-verification")
-
-        return (template, data, content_type)
-
+            for code in self.mvlist:
+                self.env.log.debug("add_meta: " + code)
+                add_meta(req, code, name="google-site-verification")
+        return template, data, content_type
